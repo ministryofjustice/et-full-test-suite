9fff880d01411854071bcd3e6dccba654602b5b3
Sivvoy Taing <sivvo@yahoo.com>
2019-01-30 10:19:02 +0000

    remove tester_server and gitmodules

--- docker/test_server/s3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# `/sbin/setuser memcache` runs the given command as the user `memcache`.
-# If you omit that part, the command will be run as root.
-export MINIO_ACCESS_KEY=accessKey1
-export MINIO_SECRET_KEY=verySecretKey1
-exec /sbin/setuser app minio server /home/app/minio_data  >>/var/log/s3.log 2>&1
