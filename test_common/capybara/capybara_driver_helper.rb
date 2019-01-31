b25692edd8bfad1e3107d44cd9437548237884fb
Sivvoy Taing <sivvo@yahoo.com>
2019-01-31 11:45:12 +0000

    add drivers for travis

--- test_common/capybara/capybara_driver_helper.rb
+++ test_common/capybara/capybara_driver_helper.rb
@@ -26,6 +26,14 @@ Capybara.register_driver :chromedriver do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
+Capybara.register_driver :chromedriver_travis do |app|
+  Capybara::Selenium::Driver.new(app, browser: :remote, desired_capabilities: :chrome, url: ENV.fetch('SELENIUM_URL', 'http://localhost:4444/wd/hub'))
+  options.add_argument('--no-sandbox')
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
 Capybara.register_driver :chromedriver_headless do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--no-sandbox')
